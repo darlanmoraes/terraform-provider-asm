2a9455b536cf442f889a88a046e87b53bf32c515
Darlan Silveira <darlan.silveira@gravicore.io>
2024-08-23 00:27:37 -0300

    Initial resources

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return &schema.Provider{
+				Schema: map[string]*schema.Schema{
+					"region": {
+						Type:        schema.TypeString,
+						Required:    true,
+						DefaultFunc: schema.EnvDefaultFunc("AWS_REGION", nil),
+						Description: "The region where AWS operations will take place.",
+					},
+				},
+				ResourcesMap: map[string]*schema.Resource{
+					"asm_appsync_graphql_api":            resourceAsmAppSyncGraphQLApi(),
+					"asm_appsync_merged_api_association": resourceAsmAppSyncMergedApiAssociation(),
+					"asm_appsync_start_schema_merge":     resourceAsmAppSyncStartSchemaMerge(),
+				},
+				ConfigureFunc: configureFunc,
+			}
+		},
+	})
+}
